46bd2f701ac0db2fb9e97de6e1c8ab9e74963555
shana nasrin <shananasrin@shanas-MacBook-Air.local>
2024-12-20 18:59:44 +0530

    key changed

--- login page/login.js
+++ login page/login.js
@@ -1,24 +1,26 @@
 // Initialize Supabase client
 const supabaseUrl = 'https://wtrxaghpwedjfmesareb.supabase.co'; 
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0cnhhZ2hwd2VkamZtZXNhcmViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQyNDY2NzUsImV4cCI6MjA0OTgyMjY3NX0.MeJyDeSmIWkNR9JGbPUTW45HNFu42zz3xg7S90pEyvo'; // Replace with your actual Supabase key
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind0cnhhZ2hwd2VkamZtZXNhcmViIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNDI0NjY3NSwiZXhwIjoyMDQ5ODIyNjc1fQ.d1GZ11sNYbzj46NGZqg-uWhZHHC2mICEaiiOYM6TvCQ'; // Replace with your actual Supabase key
 const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
 
 document.getElementById('loginForm').addEventListener('submit', async function (event) {
   event.preventDefault();
+  
 
   // Get form data
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
-
+  
   const { data, error } = await supabase
   .from('register')
-  .select('email,password')
-  .eq('email',email)
-  .eq('password',password)
+  .select('*')
+  .eq('email', email)
+  .eq('password', password)
   .single();
- 
   
-  console.log('All Users:', data);
+  
+  console.log('Full response from Supabase:', { data, error });
+  console.log('Users:', data);
 
   // Handle errors and success
   if (error || !data) {
